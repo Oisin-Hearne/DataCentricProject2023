1778bd5eeced2962b43ff7ad145123dd047d2d7d
Oisin Hearne <oisinhearne@gmail.com>
2023-12-31 21:36:05 +0000

    Home page made, dao updated w/ Mongo & MySQL

--- dao.js
+++ dao.js
@@ -1,13 +1,55 @@
 const pmysql = require('promise-mysql')
+const mongo = require('mongodb').MongoClient
 var pool;
 
-// Database setup
+
+
+///MONGODB
+MongoClient.connect('mongodb://127.0.0.1:27017')
+    .then((client) => {
+        db = client.db('proj2023MognoDB')
+        coll = db.collection('managers')
+    })
+    .catch((error) => {
+        console.log(error.message)
+    })
+
+//Get Managers from MongoDB
+var getManagers = function() {
+    return new Promise((resolve, reject) => {
+        var cursor = coll.find()
+        cursor.toArray()
+            .then((documents) => {
+                resolve(documents)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+//Add a manager to the database.
+var addManager = function(manager) {
+    return new Promise((resolve, reject) => {
+        coll.insertOne(employee)
+            .then((documents) => {
+                resolve(documents)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+
+
+///MYSQL
 pmysql.createPool({
     connectionLimit: 3,
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'studentdb4'
+    database: 'proj2023'
 })
     .then(p => {
         pool = p
@@ -17,10 +59,10 @@ pmysql.createPool({
     });
 
 
-// Returns all of the students from the student_table in the database
-var getStudents = function() {
+//Get Stores
+var getStores = function () {
     return new Promise((resolve, reject) => {
-        pool.query('select * from student_table')
+        pool.query('select * from store')
             .then((data) => {
                 resolve(data)
             })
@@ -30,14 +72,27 @@ var getStudents = function() {
     })
 }
 
-// Deletes a student from the student_table
-var deleteStudent = function(studentID) {
-    var deletionQuery = {
-        sql: 'delete from student_table where student_id=?',
-        values: [studentID]
+//Edit Store
+var editStore = function (storeID, location, mgrid) {
+    var editQuery = {
+        sql: 'update store set location=?, mgrid=?, where sid=?',
+        values: [location, mgrid, storeID]
     }
     return new Promise((resolve, reject) => {
-        pool.query(deletionQuery)
+        pool.query(editQuery)
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+//Get Products
+var getProducts = function () {
+    return new Promise((resolve, reject) => {
+        pool.query('select * from product')
             .then((data) => {
                 resolve(data)
             })
@@ -47,4 +102,44 @@ var deleteStudent = function(studentID) {
     })
 }
 
-module.exports = {getStudents, deleteStudent};
+// Deletes a product from the product table.
+var deleteProduct = function (productID) {
+    var deletionQuery = {
+        sql: 'delete from product where pid=?',
+        values: [productID]
+    }
+    if(checkProduct(productID))
+        return new Promise((resolve, reject) => {
+            pool.query(deletionQuery)
+                .then((data) => {
+                    resolve(data)
+                })
+                .catch(error => {
+                    reject(error)
+                })
+        })
+    else
+        return new Promise((resolve, reject) => {
+            reject("Not found!")
+        })
+}
+
+//Checks for a product's existence in the product_store - ie, is the product on shelves?
+var checkProduct = function (productID) {
+    var checkQuery = {
+        sql: 'select * from product_store where pid=?',
+        values: [productID]
+    }
+    return new Promise((resolve, reject) => {
+        pool.query(checkQuery)
+            .then((data) => {
+                console.log("Checking: "+data)
+                resolve(data)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+module.exports = { getManagers, addManager, getStores, editStore, getProducts, deleteProduct };
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const SQLDAO = require('./dao')
+const DBDAO = require('./dao')
 const app = express()
 const { check, validationResult } = require('express-validator')
 let ejs = require('ejs')
@@ -9,22 +9,49 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 const port = 3004
 
-
+//Home Page
 app.get('/', (req, res) => {
-    SQLDAO.getStudents()
+    res.render("home");
+})
+
+//Stores
+
+//Update a store & return user to /stores.
+//Conditions: SID can't be edited, Location must be 1 character at least, Manager ID must be 4chars and must exist.
+
+//Add a store, same conditions as above.
+
+
+//Products
+
+//Delete a product - can't delete if it's not in any store.
+
+
+//Managers
+
+//Add Manager - ID must be unique & at least 4chars, name must be at least 5chars, Salary must be >30000 & <70000
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
+//Code Snippets
+/*
+Send user to students.ejs
+    DBDAO.getStudents()
         .then((data) => {
-            res.render("student", { "students": data }); //The render line sends the user to the student.ejs
+            res.render("student", { "students": data }); 
         })
         .catch((error) => {
             console.log(error)
         });
-})
 
+//Delete a student from the db
 app.get('/students/delete/:id', async (req, res) => {
-    var found = 0;
-
-    //Deleting the Student
-    SQLDAO.deleteStudent(req.params.id)
+ 
+   var found = 0;
+    DBDAO.deleteStudent(req.params.id)
         .then((data) => {
             if(data.affectedRows == 1)
                 res.send("Student:"+req.params.id+" deleted!");
@@ -38,8 +65,8 @@ app.get('/students/delete/:id', async (req, res) => {
                 found = 1;
             }
         });
+
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+
+*/
